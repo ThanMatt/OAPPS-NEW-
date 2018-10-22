bbce89e039c92b6b0a3edd57ec1a40948300abb6
Aethan Matthew <belphegorous@gmail.com>
2018-10-22 22:57:24 +0800

    fixed admin logo

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -1,6 +1,10 @@
 <?php 
 $account_id = $this->session->userdata('account_id'); 
-$prefix = $this->session->userdata('prefix'); 
+if ($account_id == 'TS_BITS') {
+  $prefix = 'BITS';
+} else {
+  $prefix = $this->session->userdata('prefix'); 
+}
 $organization = $this->session->userdata('organization');
 $full_name = $this->session->userdata('full_name');
 $position = $this->session->userdata('position');
