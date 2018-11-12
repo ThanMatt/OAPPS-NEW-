f58d52b585d66c5344a6038b937dc865129b9520
Aethan Matthew <belphegorous@gmail.com>
2018-11-12 21:25:26 +0800

    FPDF library added

---
+++ application/controllers/Printing.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Printing extends CI_Controller {
+  public function budget_proposal($proposal_id) {
+    $record = $this->proposals_model->viewOERecord($proposal_id);
+    $data['records'] = $record;
+    $this->load->view('print/bp', $data);
+  }
+}
+
+?>
