18f2a198ad5441dfc1b115f0df4c57d6348e886c
Aethan Matthew Ilagan <aethanilagan@gmail.com>
2018-11-20 23:27:36 +0800

    Add files via upload

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -10,7 +10,7 @@ class Admin extends CI_Controller {
 
 
     $data['logs'] = $this->logs_model->showLogs();
-    $this->load->view('admin_main', $data);
+    $this->load->view('admin/admin_main', $data);
 
   }
 
@@ -37,6 +37,33 @@ class Admin extends CI_Controller {
     $data['logs'] = $this->logs_model->showLogs();
     $this->load->view('layouts/display_logs', $data);
   }
+
+  public function add() {
+
+    $this->load->view('admin/admin_add');
+
+  }
+
+  public function edit() {
+
+    $data['accounts'] = $this->accounts_model->getAllAccounts();
+    $this->load->view('admin/admin_edit', $data);
+
+  }
+
+  public function showAccount() {
+
+    $account_id = $this->input->post('account_id', true);
+    $record = $this->admin_model->viewAccountInfo($account_id);
+
+    $data['records'] = null;
+
+    if ($record) {
+      $data['records'] = $record;
+      $response = $this->load->view('layouts/display_edit', $data);
+    }
+  }
+
 
 }
 
